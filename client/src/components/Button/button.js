897fd213f366a158448e01c960020a09d395210a
Dipendra <dipshi8323@gmail.com>
2022-02-14 18:26:28 -0800

    removed unnecessary files and and spaces

--- client/src/components/Button/button.js
+++ client/src/components/Button/button.js
@@ -1,7 +1,7 @@
 import React from 'react'
 function Button(props) {
     return (
-        <Button>{props.children}</Button>
+        <button>{props.children}</button>
     )
 }
 export default Button
