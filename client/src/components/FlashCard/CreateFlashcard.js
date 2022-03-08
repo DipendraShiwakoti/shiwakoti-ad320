0b0a655a7f13b4a0b5b201786faf1dd930c7c57d
Dipendra <dipshi8323@gmail.com>
2022-03-07 16:02:26 -0800

    completed validation process

--- client/src/components/FlashCard/CreateFlashcard.js
+++ client/src/components/FlashCard/CreateFlashcard.js
@@ -1,50 +1,69 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 import { Button, Stack, TextField } from "@mui/material"
 import axios from 'axios'
 
 const CreateFlashcard = ({ userId, deckId }) => {
-  // how can we use state here to make sure we're validating info
   console.log(`[CreateFlashcard] deckId is ${deckId}`)
-  const [formValue, setFormValue] = useState({
-  frontImage: "",
-  frontText: "",
-  backImage: "",
-  backText: ""
-})
-const [err, setErrs] = useState({
-  'frontImage': false,
-  'frontText': false,
-  'backImage': false,
-  'backText': false
+  const initialValues = {frontImage: "", frontText: "", backImage: "", backText: ""}
+  const [formValue, setFormValue] = useState({initialValues 
+  })
+  const[formErrors, setFormErrors] = useState({})
+  const[isSubmit, setIsSubmit] = useState(false)
+  
 
-})
+const validate = (values) => {
+  const errors = {}
+  const re = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
 
+  if(!values.frontImage){
+  errors.frontImage = "front Image is requred"
+} else if (!re.test(values.frontImage)){
+  errors.frontImage = "This is not a valid Image formmat"
+}
 
+if(!values.frontText){
+  errors.frontText= "front Image is requred"
+}
+
+if(!values.backImage){
+  errors.backImage = "front Image is requred"
+} else if (!re.test(values.backImage)){
+  errors.backImage = "This is not a valid Image formmat"
+}
+
+if(!values.backText){
+  errors.backText = "front Image is requred"
+}
+return errors;
+
+}
   const handleChange = (event) => {
     event.preventDefault()
     console.log("[CreateFlashcard] onChange ", event)
-    const currentValues = formValue
-    currentValues[event.target.name] = event.target.value
-    setFormValue(currentValues)
+   const {name, value } = event.target
+   setFormValue({ ...formValue, [name]: value})
+   console.log(formValue)
   }
   
   const handleSubmit = async (event) => {
     console.log("[CreateFlashcard] onSubmit ", event)
     event.preventDefault()
-    const card = {
-      // frontImage,
-      // frontText,
-      // backImage,
-      // backText
-    }
+    setFormErrors(validate(formValue))
+    setIsSubmit(true)
     try {
       const response = await axios.post(`http://localhost:8000/decks/${deckId}/cards`, formValue, { headers: { user: userId } })
+      //<pre>{JSON.stringify(formValue, undefined, 2)} </pre>
       console.log(`[createflashcard] response submit ${response.status}`)
     } catch (err) {
       console.log(`response error ${err.status}`)
-      //handle th
     }
   }
+  useEffect(() => {
+    console.log(formErrors)
+    if(Object.keys(formErrors).length === 0 && isSubmit){
+      console.log(formValue)
+    }
+  }, [formErrors])
 
   return (
     <Stack component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
