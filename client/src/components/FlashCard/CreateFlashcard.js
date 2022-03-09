c98a32cb26c82e7be327b9bb9470e3f2440f73ec
Dipendra <dipshi8323@gmail.com>
2022-03-09 01:42:36 -0800

    made changes

--- client/src/components/FlashCard/CreateFlashcard.js
+++ client/src/components/FlashCard/CreateFlashcard.js
@@ -1,71 +1,58 @@
-import React, {useState, useEffect} from "react"
+import React, {useState} from "react"
 import { Button, Stack, TextField } from "@mui/material"
 import axios from 'axios'
 
 const CreateFlashcard = ({ userId, deckId }) => {
   console.log(`[CreateFlashcard] deckId is ${deckId}`)
-  const initialValues = {frontImage: "", frontText: "", backImage: "", backText: ""}
-  const [formValue, setFormValue] = useState({initialValues 
+  const [formValue, setFormValue] = useState({ frontText:"",frontImage: "", backImage: "",backText: ""})
+  const[formErrors, setFormErrors] = useState({
+    frontImage: false,
+    backImage: false,
+    frontText: false,
+    backText: false
   })
-  const[formErrors, setFormErrors] = useState({})
-  const[isSubmit, setIsSubmit] = useState(false)
   
-const validate = (values) => {
-  const errors = {}
+const validate = (name,value) => {
   const re = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
-
-  if(!values.frontImage){
-  errors.frontImage = "front Image is requred"
-} else if (!re.test(values.frontImage)){
-  errors.frontImage = "This is not a valid Image formmat"
-}
-
-if(!values.frontText){
-  errors.frontText= "front Image is requred"
-}
-
-if(!values.backImage){
-  errors.backImage = "front Image is requred"
-} else if (!re.test(values.backImage)){
-  errors.backImage = "This is not a valid Image formmat"
-}
-
-if(!values.backText){
-  errors.backText = "front Image is requred"
-}
-return errors;
-}
-
-  const handleChange = (event) => {
-    event.preventDefault()
-    console.log("[CreateFlashcard] onChange ", event)
-   const {name, value } = event.target
-   setFormValue({ ...formValue, [name]: value})
-   console.log(formValue)
+  if (name.includes("Image")) {
+    if (!re.test(value) || !value) {
+      setFormErrors({ ...formErrors,[name]: value})
+    } else {
+      return true
+    }
+  } else {
+    return null
   }
+}
+
+const handleChange = (event) => {
+  event.preventDefault()
+  console.log("[CreateFlashcard] onChange ", event)
+  const { name, value } = event.target
+  validate(name, value)
+  setFormValue({ ...formValue, [name]: value})
+  console.log(formValue)
+}
 
   const handleSubmit = async (event) => {
     console.log("[CreateFlashcard] onSubmit ", event)
     event.preventDefault()
-    setFormErrors(validate(formValue))
-    setIsSubmit(true)
-    try {
-      const response = await axios.post(`http://localhost:8000/decks/${deckId}/cards`, formValue, { headers: { user: userId } })
-      //<pre>{JSON.stringify(formValue, undefined, 2)} </pre>
-      console.log(`[createflashcard] response submit ${response.status}`)
-    } catch (err) {
-      console.log(`response error ${err.status}`)
+    if (formErrors.frontImage || formErrors.backImage || formErrors.frontText || formErrors.backText) {
+    } else {
+      alert("invalid entries")
+      try {
+        const response = await axios.post(`http://localhost:8000/decks/${deckId}/cards`, formValue, { headers: { user: userId } })
+        //<pre>{JSON.stringify(formValue, undefined, 2)} </pre>
+        console.log(`[createflashcard] response submit ${response.status}`)
+      } catch (err) {
+        console.log(`response error ${err.status}`)
+      }
     }
   }
-  useEffect(() => {
-    console.log(formErrors)
-    if(Object.keys(formErrors).length === 0 && isSubmit){
-      console.log(formValue)
-    }
-  }, [formErrors])
 
   return (
     <Stack component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+    <pre>{JSON.stringify(formValue,undefined, 2)} </pre>
       <span>Form values: {formValue.frontText} &amp; {formValue.backText}</span>
       <TextField
         margin="normal"
@@ -76,6 +63,8 @@ return errors;
         name="frontImage"
         onChange={handleChange}
         autoFocus
+        error={formErrors.frontImage}
+        //label = {formErrors.frontImage}
       />
       <TextField
         margin="normal"
