7ca2365e29443d1e2021d380802db461674a34da
Dipendra <dipshi8323@gmail.com>
2022-03-16 14:35:21 -0700

    made some changes

--- client/src/components/Flashcard/CreateFlashcard.js
+++ client/src/components/Flashcard/CreateFlashcard.js
@@ -1,58 +1,100 @@
-import React, {useState} from "react"
-import { Button, Stack, TextField } from "@mui/material"
+import React, {useEffect, useState} from "react"
+import { Button, Stack, TextField, Container, MenuItem } from "@mui/material"
 import axios from 'axios'
+import { useAuth } from "../Auth/AuthProvider"
 
 const CreateFlashcard = ({ userId, deckId }) => {
-  console.log(`[CreateFlashcard] deckId is ${deckId}`)
-  const [formValue, setFormValue] = useState({ frontText:"",frontImage: "", backImage: "",backText: ""})
-  const[formErrors, setFormErrors] = useState({
+  const { auth } = useAuth()
+  const [formValue, setFormValue] = useState({})
+  const [formErrors, setFormErrors] = useState({
     frontImage: false,
     backImage: false,
     frontText: false,
     backText: false
   })
-  
-const validate = (name,value) => {
-  const re = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
-  if (name.includes("Image")) {
-    if (!re.test(value) || !value) {
-      setFormErrors({ ...formErrors,[name]: value})
-    } else {
-      setFormValue({...formValue,[name]:value})
+  const [decks, setDecks] = useState(null)
+
+  useEffect(() => {
+    if (auth) {
+      axios.get(`http://localhost:8000/users/${auth.user}`, { headers: { authorization: `Bearer ${auth.token}` }}).then((response) => {
+        const userDecks = response.data.decks.map((deck) => {
+          return {
+            id: deck._id,
+            name: deck.name
+          }
+        })
+        setDecks(userDecks)
+        setFormValue(f => f.deck = userDecks[0].id)
+      })
     }
-  } else {
-    return null
+  }, [auth])
+
+  const isURL = (value) => {
+    const re = /[(http(s)?)://(www.)?a-zA-Z0-9@:%._+~#=]{2,256}.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/
+    return re.test(value)
   }
-}
 
-const handleChange = (event) => {
-  event.preventDefault()
-  console.log("[CreateFlashcard] onChange ", event)
-  const { name, value } = event.target
-  validate(name, value)
-  setFormValue({ ...formValue, [name]: value})
-  console.log(formValue)
-}
+  const isNotEmpty = (value) => {
+    const trimmed = value.trim()
+    return trimmed !== ""
+  }
 
+  const validate = (field, value) => {
+    const validations = {
+      frontImage: isURL,
+      backImage: isURL,
+      frontText: isNotEmpty,
+      backText: isNotEmpty,
+      deck: (v) => { return true }
+    }
+    return validations[field](value)
+  }
+
+  const handleChange = (event) => {
+    // TIL there are ways to use functions to accomplish state change
+    setFormValue(f => ({
+      ...f,
+      [event.target.name]: event.target.value
+    }))
+
+    // which isn't much different than this, but is still kind of cool
+    setFormErrors({ ...formErrors, [event.target.name]: !validate(event.target.name, event.target.value)})
+  }
+  
   const handleSubmit = async (event) => {
-    console.log("[CreateFlashcard] onSubmit ", event)
     event.preventDefault()
-    if (formErrors.frontImage || formErrors.backImage || formErrors.frontText || formErrors.backText) {
-      alert('Error adding card')
-    } else {
+    if (!formErrors.frontImage && !formErrors.frontText && !formErrors.backImage && !formErrors.backText) {
       try {
-        const response = await axios.post(`http://localhost:8000/decks/${deckId}/cards`, formValue, { headers: { user: userId } })
-        console.log(`[createflashcard] response submit ${response.status}`)
+        const response = await axios.post(`http://localhost:8000/decks/${deckId}/cards`, formValue, { headers: { authorization: `Bearer ${auth.token}`} })
       } catch (err) {
-        console.log(`response error ${err.status}`)
+        alert("Submission failed!")
       }
     }
   }
 
+  if (!decks) {
+    return <span>Loading...</span>
+  }
+
   return (
+    <Container maxWidth="md">
     <Stack component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
-    <pre>{JSON.stringify(formValue,undefined, 2)} </pre>
-      <span>Form values: {formValue.frontText} &amp; {formValue.backText}</span>
+    <TextField
+          id="deck"
+          name="deck"
+          select
+          label="Deck"
+          value={formValue.deck ?? ''}
+          helperText="Please select your currency"
+          onChange={handleChange}
+          autoFocus
+        >
+          {decks.map((deck) => (
+            <MenuItem key={deck.id} value={deck.id}>
+              {deck.name}
+            </MenuItem>
+          ))}
+        </TextField>
       <TextField
         margin="normal"
         required
@@ -61,7 +103,6 @@ const handleChange = (event) => {
         label="Front Image"
         name="frontImage"
         onChange={handleChange}
-        autoFocus
         error={formErrors.frontImage}
       />
       <TextField
@@ -72,8 +113,7 @@ const handleChange = (event) => {
         label="Front Text"
         id="frontText"
         onChange={handleChange}
-        autoFocus
-        error ={formErrors.frontText}
+        error={formErrors.frontText}
       />
       <TextField
         margin="normal"
@@ -83,8 +123,7 @@ const handleChange = (event) => {
         label="Back Image"
         name="backImage"
         onChange={handleChange}
-        autoFocus
-        error = {formErrors.backImage}
+        error={formErrors.backImage}
       />
       <TextField
         margin="normal"
@@ -94,13 +133,13 @@ const handleChange = (event) => {
         label="Back Text"
         id="backText"
         onChange={handleChange}
-        autoFocus
-        error ={formErrors.backText}
+        error={formErrors.backText}
       />
-      <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+      <Button type="submit" disabled={ Object.values(formErrors).every(value=>value===false) } fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
         Submit
       </Button>
     </Stack>
+    </Container>
   )
 }
 
